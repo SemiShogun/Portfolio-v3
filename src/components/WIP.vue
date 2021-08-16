80fb3e0c75e30f5f60a731ca68474ee960c8e582
SemiShogun <nectoj@gmail.com>
2021-08-16 13:26:14 +0200

    🚀Add WIP site

---
+++ src/components/WIP.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="wip">
+    <h1>Nectoj.me is under construction</h1>
+    <img
+      id="construction"
+      alt="Construction Icon"
+      src="../assets/under_construction.png"
+    />
+    <br />
+    <h3>This page is still under construction, please come back later :)</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from "vue";
+
+export default defineComponent({
+  name: "WIP",
+});
+</script>
+
+<style lang="scss">
+#construction {
+  width: 20%;
+}
+</style>
