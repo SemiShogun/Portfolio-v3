3b1a575c55250c5b871f2d55f00e89fb8eb7f39f
SemiShogun <nectoj@gmail.com>
2021-08-21 16:45:52 +0200

    🔧Add Navbar

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header>
-    <slot />
+    <Navbar />
+    <div class="slot">
+      <slot />
+    </div>
+    <Footer />
   </div>
 </template>
 
 <script lang="ts">
-export default {};
+import { defineComponent } from '@vue/composition-api'
+import Navbar from './Navbar.vue';
+import Footer from './Footer.vue';
+
+export default defineComponent({
+  components: { Navbar, Footer },
+})
 </script>
 
 <static-query>
@@ -25,31 +26,14 @@ query {
 }
 </static-query>
 
-<style>
-body {
-  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
-    "Helvetica Neue", Arial, sans-serif;
-  margin: 0;
-  padding: 0;
-  line-height: 1.5;
-}
-
+<style lang="scss" scoped>
 .layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
   align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
+  justify-content: center;
 }
 
-.nav__link {
-  margin-left: 20px;
+.slot {
 }
 </style>
