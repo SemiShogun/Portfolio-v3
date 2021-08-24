5ef14fdbeb375404d387a56740e2a8c996b1d41e
SemiShogun <nectoj@gmail.com>
2021-08-25 00:23:58 +0200

    🔧Add blogs section for viewing blogposts

---
+++ src/pages/Blogs.vue
@@ -0,0 +1,43 @@
+<template>
+  <Layout id="blogs">
+    <div class="w-full min-h-screen flex flex-col justify-start items-center">
+      <h1 class="text-xl font-semibold mb-5">Recent Blogs</h1>
+      <BlogPost
+        v-for="edge in $page.allBlog.edges"
+        :key="edge.node.id"
+        :blog="edge.node"
+      />
+    </div>
+  </Layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+import BlogPost from '../components/Blogpost.vue'
+
+export default defineComponent({
+  metaInfo: {
+    title: 'Blogs'
+  },
+  components: { BlogPost },
+})
+</script>
+
+<page-query>
+query {
+  allBlog {
+    totalCount
+    edges {
+      node {
+        id
+        title
+        image
+        description
+        categories
+        date (format: "D MMMM YYYY")
+        path
+        }
+    }  
+  }
+}
+</page-query>
